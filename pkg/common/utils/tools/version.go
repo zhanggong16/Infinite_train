5ffdc0992c188c9487cb0eaee963d88ff61d4141
zhanggong1 <zhanggong1@jd.com>
2020-10-27 17:43:27 +0800

    add show version

---
+++ pkg/common/utils/tools/version.go
@@ -0,0 +1,16 @@
+package tools
+
+import "fmt"
+
+type VersionInfo struct {
+	ReleaseVersion string
+	Version        string
+	Compile        string
+}
+
+func ShowVersion(versionInfo *VersionInfo, banner string) {
+	fmt.Printf("%s\n", banner)
+	fmt.Printf("Release version:%s\n", versionInfo.ReleaseVersion)
+	fmt.Printf("Git commit:%s\n", versionInfo.Version)
+	fmt.Printf("Build time:%s\n", versionInfo.Compile)
+}
---
+++ pkg/executes/manager/manager.go
@@ -0,0 +1,23 @@
+package manager
+
+import (
+	"Infinite_train/pkg/common/utils/tools"
+	"flag"
+)
+
+func Main(versionInfo *tools.VersionInfo) {
+	var banner = "welcome to infinite train manager !"
+
+	//var cfgFile = flag.String("config", "/etc/manager.toml", "manager configure file absolute path!")
+	var isShowVersion = true
+	flag.BoolVar(&isShowVersion, "version", false, "Show version")
+	flag.Parse()
+
+	if isShowVersion {
+		tools.ShowVersion(versionInfo, banner)
+		return
+	}
+
+
+	return
+}
