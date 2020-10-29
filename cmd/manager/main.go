2737007d06c592bf0c1efe38fa1b965700814021
zhanggong1 <zhanggong1@jd.com>
2020-10-29 10:40:46 +0800

    change utils tools path

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"Infinite_train/pkg/common/utils/tools"
+	"Infinite_train/pkg/common/utils"
 	"Infinite_train/pkg/executes/manager"
 	"Infinite_train/pkg/hack"
 )
 
 func main() {
-	versionInfo := &tools.VersionInfo{ReleaseVersion: hack.ReleaseVersion, Version: hack.Version, Compile: hack.Compile}
+	versionInfo := &utils.VersionInfo{ReleaseVersion: hack.ReleaseVersion, Version: hack.Version, Compile: hack.Compile}
 	manager.Main(versionInfo)
 	return
 }
