5e4517eced9637e101a34ae9c8d7f5ce3f384d88
zhanggong1 <zhanggong1@jd.com>
2020-11-03 18:07:14 +0800

    add rpc func

---
+++ pkg/common/context/rpc_context.go
@@ -0,0 +1,6 @@
+package context
+
+type ReportHeatBeatRequest struct {
+	RequestId	string
+	AgentIp		string
+}
--- pkg/manager/api/rpc/manager.go
+++ pkg/manager/api/rpc/manager.go
@@ -1,8 +1,17 @@
 package rpc
 
+import (
+	"Infinite_train/pkg/common/context"
+	"Infinite_train/pkg/common/utils/log/golog"
+)
+
 type ManagerRPC struct {
 }
 
-func (rpc *ManagerRPC) ReportHeatBeat() error {
+func (rpc *ManagerRPC) ReportHeatBeat(req *context.ReportHeatBeatRequest, reply *string) error {
+	*reply = "okay"
+	requestId := req.RequestId
+
+	golog.Infof(requestId, "ReportHeatBeat, req [%+v], reply [%+v]", *req, *reply)
 	return nil
 }
