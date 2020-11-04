63cf9a8c1ee187f2d86bd8d296c6dd664b8a856e
zhanggong1 <zhanggong1@jd.com>
2020-11-04 16:17:46 +0800

    add agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,1 +1,12 @@
 package agent
+
+import (
+	"Infinite_train/pkg/common/utils"
+	"Infinite_train/pkg/executes/agent"
+	"Infinite_train/pkg/hack"
+)
+
+func main() {
+	versionInfo := &utils.VersionInfo{ReleaseVersion: hack.ReleaseVersion, Version: hack.Version, Compile: hack.Compile}
+	agent.Main(versionInfo)
+}
